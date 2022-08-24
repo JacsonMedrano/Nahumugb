74dd13b5cb4700d7c28812bfd334a83c58d2f77c
JacsonMedrano <110192151+JacsonMedrano@users.noreply.github.com>
2022-08-23 21:29:41 -0600

    Add files via upload

--- clase/ejercicio de la semana 4/extra parcial/extra parcial/Program.cs
+++ clase/ejercicio de la semana 4/extra parcial/extra parcial/Program.cs
@@ -10,6 +10,67 @@ namespace extra_parcial
     {
         static void Main(string[] args)
         {
+            
+            string[,] tabla = new string[3, 3];
+           
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if (y == 0)
+                    {
+                        Console.Write("Ingrese el nombre: ");
+                        tabla[x, y] = Console.ReadLine();
+                    }
+                    else if (y == 1)
+                    {
+                        Console.Write("Ingrese el edad ");
+                        tabla[x, y] = Console.ReadLine();
+                    }
+                    else if (y == 2) ;
+                    {
+                        Console.Write("Ingrese el carrera: ");
+                        tabla[x, y] = Console.ReadLine();
+                    }
+
+
+
+
+
+                }
+                Console.WriteLine();
+
+
+            }
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("mostrando en pantalla Nombre, edad y carrera");
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    Console.WriteLine(tabla[x, y] + "      ");
+                }
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+
+
+            
+
+            
+
+
+
+
         }
     }
-}
+}  
+
+           
+
+
+
+
