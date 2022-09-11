05f8f279d8e1a2f40120cd599d7331555d360f7e
JacsonMedrano <110192151+JacsonMedrano@users.noreply.github.com>
2022-09-11 13:15:17 -0600

    Add files via upload

---
+++ ejercicio 1 clase/ejercicio 1 clase/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio_1_clase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void boton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola Mundo");
+            label1.Text = ("Hola Mundo");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
